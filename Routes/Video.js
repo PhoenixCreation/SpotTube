5824b3685fddc21431e84041c1449b7667c5a04b
PhoenixCreation <phs28052001@gmail.com>
2021-03-23 20:38:15 +0530

    Starting :arrow_forward: Video controller of video

--- Routes/Video.js
+++ Routes/Video.js
@@ -1,12 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { StyleSheet, Text, View, Dimensions, Pressable } from "react-native";
 import { Video } from "expo-av";
-/** config.js contains this values
-  export const theme = {
-  backgroundColor: "#f1f1f1",
-  fontColor: "#212121",
-};
-*/
 import { theme } from "../config";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import Animated, {
@@ -17,6 +11,7 @@ import Animated, {
   interpolate,
   runOnJS,
 } from "react-native-reanimated";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 
 import * as MediaLibrary from "expo-media-library";
 
@@ -31,6 +26,11 @@ const BREAK_POINT = 100;
 const Videos = () => {
   const video = useRef(null);
   const [status, setStatus] = useState({});
+  const [playbackControls, setPlaybackControls] = useState(true);
+
+  const togglePlaybackControls = () => {
+    setPlaybackControls((last) => setPlaybackControls(!last));
+  };
 
   // useEffect(() => {
   //   getVideos();
@@ -82,7 +82,7 @@ const Videos = () => {
           translateY.value = withTiming(-(height - BAR_HEIGHT));
           offsetY.value = -(height - BAR_HEIGHT);
 
-          state.value = "full";
+          // state.value = "full";
         }
       }
     },
@@ -113,7 +113,7 @@ const Videos = () => {
     };
   });
 
-  const videoStyle = useAnimatedStyle(() => {
+  const videoPlaybackContStyle = useAnimatedStyle(() => {
     return {
       width:
         translateY.value < -BREAK_POINT
@@ -124,17 +124,28 @@ const Videos = () => {
               [width, width / 2]
             ),
       height: "100%",
+      position: "relative",
     };
   });
+
+  const goUp = () => {
+    translateY.value = withTiming(-(height - BAR_HEIGHT));
+    offsetY.value = -(height - BAR_HEIGHT);
+    state.value = "up";
+  };
+
+  const goDown = () => {
+    translateY.value = withTiming(0);
+    offsetY.value = 0;
+    state.value = "down";
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.mainCont}>
         <Pressable
           onPress={() => {
-            translateY.value = withTiming(-(height - BAR_HEIGHT));
-            offsetY.value = -(height - BAR_HEIGHT);
-            state.value = "up";
+            goUp();
           }}
         >
           <Text>Go full screen</Text>
@@ -144,29 +155,124 @@ const Videos = () => {
         <View style={styles.gestureCont}>
           <PanGestureHandler onGestureEvent={onGestureEvent}>
             <Animated.View style={playerContStyle}>
-              {/* Here there should a oone more container which will be reposnsible for size variations */}
-              <AnimatedVideo
-                ref={video}
-                style={videoStyle}
-                source={{
-                  uri: "file:///storage/emulated/0/Download/bannerg004.mp4",
+              <Pressable
+                style={{ flex: 1 }}
+                onPress={() => {
+                  togglePlaybackControls();
                 }}
-                useNativeControls={true}
-                resizeMode="cover"
-                onPlaybackStatusUpdate={(newstatus) => setStatus(newstatus)}
-              />
+              >
+                <Animated.View style={videoPlaybackContStyle}>
+                  <Video
+                    ref={video}
+                    style={styles.video}
+                    source={{
+                      uri: "file:///storage/emulated/0/Download/bannerg004.mp4",
+                    }}
+                    useNativeControls={false}
+                    resizeMode="cover"
+                    onPlaybackStatusUpdate={(newstatus) => setStatus(newstatus)}
+                  />
+                  {playbackControls && (
+                    <Animated.View style={styles.playbackControlsCont}>
+                      <View style={styles.controllerHeader}>
+                        <Pressable
+                          onPress={() => {
+                            goDown();
+                          }}
+                        >
+                          <AntDesign
+                            style={styles.goDownButton}
+                            name="down"
+                            size={20}
+                            color="white"
+                          />
+                        </Pressable>
+                        <Text style={styles.controllerName} numberOfLines={1}>
+                          {
+                            "A video name will be shown here. It may be too long so be prepard for that"
+                          }
+                        </Text>
+                        <View style={styles.controllerOption}>
+                          <Entypo
+                            style={styles.controllerOptionIcon}
+                            name="dots-three-vertical"
+                            size={16}
+                            color="white"
+                          />
+                        </View>
+                      </View>
+                      <View style={styles.controllerMiddle}>
+                        <Pressable
+                          onPress={() => {
+                            if (status.positionMillis < 5001) {
+                              video?.current?.setPositionAsync(0);
+                            } else {
+                              video?.current?.setPositionAsync(
+                                status.positionMillis - 5000
+                              );
+                            }
+                          }}
+                        >
+                          <Entypo
+                            name="controller-fast-backward"
+                            size={28}
+                            color="white"
+                          />
+                        </Pressable>
+                        <Pressable
+                          onPress={() => {
+                            if (status.isPlaying) {
+                              video?.current?.pauseAsync();
+                            } else {
+                              video?.current?.playAsync();
+                            }
+                          }}
+                        >
+                          {status.isPlaying ? (
+                            <Ionicons
+                              name="ios-pause"
+                              size={36}
+                              color="white"
+                            />
+                          ) : (
+                            <Entypo
+                              name="controller-play"
+                              size={36}
+                              color="white"
+                            />
+                          )}
+                        </Pressable>
+                        <Pressable
+                          onPress={() => {
+                            if (
+                              status.positionMillis + 5000 >
+                              status.durationMillis
+                            ) {
+                              video?.current?.setPositionAsync(
+                                status.durationMillis
+                              );
+                            } else {
+                              video?.current?.setPositionAsync(
+                                status.positionMillis + 5000
+                              );
+                            }
+                          }}
+                        >
+                          <Entypo
+                            name="controller-fast-forward"
+                            size={28}
+                            color="white"
+                          />
+                        </Pressable>
+                      </View>
+                      <View style={styles.controllerProgress}></View>
+                    </Animated.View>
+                  )}
+                </Animated.View>
+              </Pressable>
             </Animated.View>
           </PanGestureHandler>
         </View>
-        <Pressable
-          onPress={() => {
-            status.isPlaying
-              ? video.current.pauseAsync()
-              : video.current.playAsync();
-          }}
-        >
-          <Text>{status.isPlaying ? "pause" : "Play"}</Text>
-        </Pressable>
       </Animated.View>
     </View>
   );
@@ -197,5 +303,40 @@ const styles = StyleSheet.create({
     width: "100%",
     borderWidth: StyleSheet.hairlineWidth,
     borderColor: "black",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  playbackControlsCont: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#0005",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  controllerHeader: {
+    height: 50,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  controllerMiddle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  controllerProgress: {
+    height: 30,
+    width: "100%",
+  },
+  controllerName: {
+    flex: 1,
+    color: "white",
+    marginHorizontal: 20,
   },
 });
